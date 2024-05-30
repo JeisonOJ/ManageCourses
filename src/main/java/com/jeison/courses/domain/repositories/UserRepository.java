bc8df9c21c6d16738f112b584b166f858f811026
Jeison <jeisonortiz1516@gmail.com>
2024-05-30 09:18:48 -0500

    add new method to find user by id and role

--- src/main/java/com/jeison/courses/domain/repositories/UserRepository.java
+++ src/main/java/com/jeison/courses/domain/repositories/UserRepository.java
@@ -1,11 +1,16 @@
 package com.jeison.courses.domain.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.jeison.courses.domain.entities.User;
+import com.jeison.courses.utils.enums.RoleUser;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long>{
+
+    public Optional<User> findByIdAndRoleUser(Long id, RoleUser roleUser);
 
 }
